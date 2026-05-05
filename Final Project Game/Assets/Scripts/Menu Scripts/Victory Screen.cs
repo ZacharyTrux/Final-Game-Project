5d412333b4d57134a191eef1d4788322b70e22f4
ZacharyTrux <ztruxillo@gmail.com>
2026-05-04 22:57:44 -0500

    Fixed Merge conflicts and now everything is in playable state

--- Final Project Game/Assets/Scripts/Menu Scripts/Victory Screen.cs
+++ Final Project Game/Assets/Scripts/Menu Scripts/Victory Screen.cs
@@ -3,18 +3,15 @@ using UnityEngine.SceneManagement;
 
 
 public class VictoryScreen : MonoBehaviour{
-    public string title = "Title Screen";
-    public string levelOne = "Forest Level";
-
     private void Start(){
         Cursor.visible = true;
     }
 
     public void Restart(){
-        SceneManager.LoadScene(levelOne);
+        SceneManager.LoadScene("Forest Level");
     }
 
     public void MainMenu(){
-        SceneManager.LoadScene(title);
+        SceneManager.LoadScene("Title Screen");
     }
 }
