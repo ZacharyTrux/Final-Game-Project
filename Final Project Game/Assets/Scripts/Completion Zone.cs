eec5854001b58d07b2ee09d5c5782390f79cfedd
ZacharyTrux <ztruxillo@gmail.com>
2026-05-17 16:14:43 -0500

    Added portal fix and Sandips level from his branch

--- Final Project Game/Assets/Scripts/Completion Zone.cs
+++ Final Project Game/Assets/Scripts/Completion Zone.cs
@@ -1,9 +1,9 @@
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
 using System.Collections;
-using System.Runtime.Serialization.Formatters;
 
-public class CompletionZone : MonoBehaviour{
+public class CompletionZone : MonoBehaviour
+{
     public GameObject player2DTarget;
     public GameObject playerTDTarget;
 
@@ -15,110 +15,155 @@ public class CompletionZone : MonoBehaviour{
     private Transform player2DTransform;
     private Transform playerTDTransform;
 
-    // animation variables
     private float alignDuration = 1.0f;
-    private float alignElasped = 0f;
-    
-    private float timeElapsed = 0f;
-    public float duration = 2f;
+    private float duration = 2f;
     private float spinSpeed = 1000f;
 
-
+    private Coroutine portalCoroutine;
 
     private void Awake(){
-        if(hintText == null){
+        if (hintText == null){
             hintText = GameObject.FindWithTag("Hint").GetComponent<TextMeshProUGUI>();
         }
     }
 
+    public void ResetZone(){
+        present2D = false;
+        presentTD = false;
+        isCompleted = false;
+    }
+
     private void OnTriggerEnter(Collider other){
         UIScript.Instance.ShowCompletionPrompt();
-        if(other.GetComponent<PlayerTopDown>() != null){
+
+        if (other.GetComponent<PlayerTopDown>() != null){
             presentTD = true;
             playerTDTransform = other.transform;
         }
-        if(other.GetComponent<Player2D>() != null){
+        if (other.GetComponent<Player2D>() != null){
             present2D = true;
             player2DTransform = other.transform;
         }
-        if(present2D && presentTD && !isCompleted){
+        if (present2D && presentTD && !isCompleted){
             hintText.text = "";
             isCompleted = true;
-            StartCoroutine(PortalAnimation());
+            portalCoroutine = StartCoroutine(PortalAnimation());
         }
     }
 
     private void OnTriggerExit(Collider other){
+        if (isCompleted){
+            return;
+        }
+
         UIScript.Instance.ResetHintText();
-        if(other.GetComponent<Player2D>() != null){
+        if (other.GetComponent<Player2D>() != null){
             present2D = false;
         }
-        if(other.GetComponent<PlayerTopDown>() != null){
+        if (other.GetComponent<PlayerTopDown>() != null){
             presentTD = false;
         }
     }
-    
+
     private IEnumerator PortalAnimation(){
-        PlayerManager.Instance.enabled = false; 
-        // move players to portal spot 
+        Rigidbody rb2D = player2DTransform.GetComponent<Rigidbody>();
+        Rigidbody rbTD = playerTDTransform.GetComponent<Rigidbody>();
+
+        rb2D.linearVelocity = Vector3.zero;
+        rb2D.angularVelocity = Vector3.zero;
+        rbTD.linearVelocity = Vector3.zero;
+        rbTD.angularVelocity = Vector3.zero;
+
+        rb2D.isKinematic = true;
+        rbTD.isKinematic = true;
+
+        player2DTransform.rotation = Quaternion.identity;
+        playerTDTransform.rotation = Quaternion.identity;
+
+        PlayerManager.Instance.enabled = false;
+
         Vector3 start2DPos = player2DTransform.position;
         Vector3 startTDPos = playerTDTransform.position;
-        while(alignElasped < alignDuration){
-            alignElasped += Time.deltaTime;
-            float progress = Mathf.Clamp01(alignElasped / alignDuration);
+        float alignElapsed = 0f;
 
-            // move players to their respective targets 
+        while (alignElapsed < alignDuration)
+        {
+            alignElapsed += Time.deltaTime;
+            float progress = Mathf.Clamp01(alignElapsed / alignDuration);
             player2DTransform.position = Vector3.Lerp(start2DPos, player2DTarget.transform.position, progress);
             playerTDTransform.position = Vector3.Lerp(startTDPos, playerTDTarget.transform.position, progress);
-
             yield return null;
         }
 
         player2DTransform.position = player2DTarget.transform.position;
         playerTDTransform.position = playerTDTarget.transform.position;
+
         yield return new WaitForSeconds(1f);
 
-        // portal animation 
         start2DPos = player2DTransform.position;
         startTDPos = playerTDTransform.position;
-        var start2DRot = player2DTransform.rotation;
-        var startTDRot = playerTDTransform.rotation;
         Vector3 portalCenter = transform.position;
 
         Vector3 startScale2D = player2DTransform.localScale;
         Vector3 startScaleTD = playerTDTransform.localScale;
+
+        Quaternion saved2DRot = Quaternion.identity;
+        Quaternion savedTDRot = Quaternion.identity;
+
+        float timeElapsed = 0f;
         SoundManager.Play(SoundType.PORTAL);
 
-        while(timeElapsed < duration){
+        while (timeElapsed < duration)
+        {
             timeElapsed += Time.deltaTime;
-
             float progress = Mathf.Clamp01(timeElapsed / duration);
             float easeProgress = Mathf.Pow(progress, 3);
 
-            player2DTransform.position = Vector3.Lerp(start2DPos, portalCenter, easeProgress);
-            playerTDTransform.position = Vector3.Lerp(startTDPos, portalCenter, easeProgress);
+            if (player2DTransform != null)
+                player2DTransform.position = Vector3.Lerp(start2DPos, portalCenter, easeProgress);
+            if (playerTDTransform != null)
+                playerTDTransform.position = Vector3.Lerp(startTDPos, portalCenter, easeProgress);
 
-            player2DTransform.localScale = Vector3.Lerp(startScale2D, Vector3.zero, easeProgress);
-            playerTDTransform.localScale = Vector3.Lerp(startScaleTD, Vector3.zero, easeProgress);
+            if (player2DTransform != null)
+                player2DTransform.localScale = Vector3.Lerp(startScale2D, Vector3.zero, easeProgress);
+            if (playerTDTransform != null)
+                playerTDTransform.localScale = Vector3.Lerp(startScaleTD, Vector3.zero, easeProgress);
 
             float currentSpin = Mathf.Lerp(0, spinSpeed, easeProgress);
-            player2DTransform.Rotate(Vector3.forward, currentSpin * Time.deltaTime);
-            playerTDTransform.Rotate(Vector3.up, currentSpin * Time.deltaTime);
+            if (player2DTransform != null)
+                player2DTransform.Rotate(Vector3.forward, currentSpin * Time.deltaTime);
+            if (playerTDTransform != null)
+                playerTDTransform.Rotate(Vector3.up, currentSpin * Time.deltaTime);
 
             yield return null;
         }
+
         SoundManager.Stop(SoundType.PORTAL);
+
+        player2DTransform.localScale = startScale2D;
+        playerTDTransform.localScale = startScaleTD;
+        player2DTransform.rotation = saved2DRot;
+        playerTDTransform.rotation = savedTDRot;
+
+        rb2D.isKinematic = false;
+        rbTD.isKinematic = false;
+
         yield return null;
 
-        // reset player transforms and complete level
-        player2DTransform.localScale = startScale2D;
-        playerTDTransform.localScale = startScaleTD;
-        player2DTransform.rotation = start2DRot;
-        playerTDTransform.rotation = startTDRot;
-        player2DTransform.localScale = startScale2D;
-        playerTDTransform.localScale = startScaleTD;
-        player2DTransform.rotation = start2DRot;
-        playerTDTransform.rotation = startTDRot;
+        Transform temp2D = player2DTransform;
+        Transform tempTD = playerTDTransform;
+        player2DTransform = null;
+        playerTDTransform = null;
+
         LevelManager.Instance.CompleteCurrSublevel();
+    }
+
+    private void OnDisable(){
+        if (portalCoroutine != null){
+            StopCoroutine(portalCoroutine);
+            portalCoroutine = null;
+        }
+        player2DTransform = null;
+        playerTDTransform = null;
     }
 }
--- Final Project Game/Assets/Scripts/Level Manager.cs
+++ Final Project Game/Assets/Scripts/Level Manager.cs
@@ -1,5 +1,6 @@
-using UnityEngine;
+﻿﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+
 
 public class LevelManager : MonoBehaviour
 {
@@ -17,102 +18,85 @@ public class LevelManager : MonoBehaviour
     private int currLevel = 0;
     private Player2D player2D;
     private PlayerTopDown playerTD;
-    private bool isTransitioning = false;
+    public bool isTransitioning = false;
+
 
     public static LevelManager Instance { get; private set; }
 
     void Awake(){
-        if (Instance != null && Instance != this)
-        {
+        if (Instance != null && Instance != this){
             Destroy(gameObject);
             return;
         }
-
         Instance = this;
     }
 
     void Start(){
+        currLevel = 0;
         player2D = GameObject.FindWithTag("2DPlayer").GetComponent<Player2D>();
         playerTD = GameObject.FindWithTag("TopDownPlayer").GetComponent<PlayerTopDown>();
 
-        // Turn off all sublevels first
-        for (int i = 0; i < subLevels.Length; i++)
-        {
-            if (subLevels[i].subLevelObjects != null)
-            {
+        for (int i = 0; i < subLevels.Length; i++){
+            subLevels[i].isCompleted = false;
+            if(subLevels[i].subLevelObjects != null){
                 subLevels[i].subLevelObjects.SetActive(false);
             }
         }
-
         SetSubLevel();
     }
 
-    public void SetSubLevel()
-    {
-        Debug.Log("Loading sublevel index: " + currLevel + " Name: " + subLevels[currLevel].name);
+    public void SetSubLevel(){
 
-        // Turn on current sublevel
-        if (subLevels[currLevel].subLevelObjects != null)
-        {
+        if (subLevels[currLevel].subLevelObjects != null){
             subLevels[currLevel].subLevelObjects.SetActive(true);
         }
-        else
-        {
-            Debug.LogError("SubLevel Objects missing for: " + subLevels[currLevel].name);
-        }
 
-        if (subLevels[currLevel].spawnPoint2D == null)
-        {
-            Debug.LogError("2D spawn point missing for: " + subLevels[currLevel].name);
-        }
+        Transform sp2D = subLevels[currLevel].spawnPoint2D;
+        Transform spTD = subLevels[currLevel].spawnPointTD;
 
-        if (subLevels[currLevel].spawnPointTD == null)
-        {
-            Debug.LogError("TopDown spawn point missing for: " + subLevels[currLevel].name);
-        }
-
-        player2D.SetSpawn(subLevels[currLevel].spawnPoint2D);
-        playerTD.SetSpawn(subLevels[currLevel].spawnPointTD);
-
-        PlayerManager.Instance.GroupRespawn();
+        player2D.SetSpawn(sp2D);
+        playerTD.SetSpawn(spTD);
+        Rigidbody rb2D = player2D.GetComponent<Rigidbody>();
+        Rigidbody rbTD = playerTD.GetComponent<Rigidbody>();
+        
+        player2D.transform.rotation = Quaternion.identity;
         PlayerManager.Instance.Setup2D();
-        ScoringManager.Instance.StartLevelTracking();
-        PlayerManager.Instance.enabled = true;
         PlayerManager.Instance.GroupRespawn();
+
+        ScoringManager.Instance.StartLevelTracking();
+
+        PlayerManager.Instance.enabled = true;
+
     }
 
-    public void CompleteCurrSublevel()
-    {
+    public void CompleteCurrSublevel(){
         if (isTransitioning) return;
 
         SubLevel curr = subLevels[currLevel];
-        if (curr.isCompleted) return;
-
         isTransitioning = true;
         curr.isCompleted = true;
 
-        if (curr.subLevelObjects != null)
-        {
-            //curr.subLevelObjects.SetActive(false);
+        if (curr.subLevelObjects != null){
+            curr.subLevelObjects.SetActive(false);
         }
 
+        int prevLevel = currLevel;
         currLevel += 1;
+
         ScoringManager.Instance.StopLevelTracking();
 
         if (currLevel <= subLevels.Length - 1)
         {
             SetSubLevel();
-            Invoke(nameof(ResetTransition), 0.5f);
+            Invoke(nameof(ResetTransition), 2f);
         }
-        else
-        {
+        else{
             Destroy(UIScript.Instance.gameObject);
             SceneManager.LoadScene("Victory Screen");
         }
     }
 
-    private void ResetTransition()
-    {
+    private void ResetTransition(){
         isTransitioning = false;
     }
 }
